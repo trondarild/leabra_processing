0d1facb2d93001e3b90c6952de8d3e181713c787
trondarild <trondarild@gmail.com>
2022-01-09 17:26:00 +0100

    added ext

--- leabra_processing_dev/mktest.sh
+++ leabra_processing_dev/mktest.sh
@@ -7,7 +7,7 @@ ORGNAME="$2"
 EXT='pde'
 ## copy everything from MyModule
 cp $ORGNAME.$EXT $MODULENAME.$EXT
-echo 'Making test file:' "$MODULENAME"
+echo 'Making test file:' "$MODULENAME"."$EXT"
 # search and replace in files
 sed -i -e 's/'"$ORGNAME"'/'"$MODULENAME"'/g'  $MODULENAME.$EXT
 echo "Done"
